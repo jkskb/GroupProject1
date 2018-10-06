02c23286103f4a5937041e92df021ad29f1bbbf7
Kate Ryan <jkskb@yahoo.com>
2018-10-05 19:27:38 -0500

    style and time added to weather

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,16 +14,17 @@
         method: "GET"
     }).then(function(response) {
 
-        console.log(queryURL1);
+        var now = moment();
+        console.log("DATE:" + moment(now).format("ddd[,] MMM Do") + " CURRENT TIME: " + moment(now).format("hh:mm a"));
 
-        console.log(response);
+        $("#date").text(now.format("ddd[,] MMM Do[ ― ]h:mm a"));
 
         var iconCode = response.weather[0].icon;
 
         var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         
         $(".city").html("<h2> " + response.name + " </h2>");
-        $(".current").text("Current: " + response.weather[0].description);
+        $(".current").text("Current ― " + response.weather[0].description);
         $(".icon").html("<img src='" + iconUrl  + "'>");
         $(".currentWind").text("Wind Speed: " + response.wind.speed + " mph");
 
@@ -42,23 +43,26 @@
       method: "GET"
     }).then(function(response) {
 
-        console.log(queryURL1);
-
-        console.log(response);
+        var three = moment().add(3, 'hours');
+        var six = moment().add(6, 'hours');
+        var nine = moment().add(9, 'hours');
+        console.log("3 Hours From Now:" + moment(three).format("hh:mm a")); 
+        console.log("6 Hours From Now:" + moment(six).format("hh:mm a")); 
+        console.log("9 Hours From Now:" + moment(nine).format("hh:mm a")); 
 
         var iconCode = response.list[1].weather[0].icon;
 
         var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         
-        $(".three").text("3 Hours: " + response.list[0].weather[0].description);
+        $(".three").text(three.format("ddd[, ]h:mm a[ ― ]") + response.list[0].weather[0].description);
         $(".threeIcon").html("<img src='http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-        $(".threeWind").text("3 hrs Wind Speed: " + response.list[0].wind.speed + " mph");
-        $(".six").text("6 Hours: " + response.list[1].weather[0].description);
+        $(".threeWind").text("Wind Speed: " + response.list[0].wind.speed + " mph");
+        $(".six").text(six.format("ddd[, ]h:mm a[ ― ]") + response.list[1].weather[0].description);
         $(".sixIcon").html("<img src='http://openweathermap.org/img/w/" + response.list[1].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-        $(".sixWind").text("6 hrs Wind Speed: " + response.list[1].wind.speed + " mph");
-        $(".nine").text("9 Hours: " + response.list[2].weather[0].description);
+        $(".sixWind").text("Wind Speed: " + response.list[1].wind.speed + " mph");
+        $(".nine").text(nine.format("ddd[, ]h:mm a[ ― ]") + response.list[2].weather[0].description);
         $(".nineIcon").html("<img src='http://openweathermap.org/img/w/" + response.list[2].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-        $(".nineWind").text("9 hrs Wind Speed: " + response.list[2].wind.speed + " mph");
+        $(".nineWind").text("Wind Speed: " + response.list[2].wind.speed + " mph");
 
 
         // console.log("3 Hours: " + response.list[0].weather[0].description);
